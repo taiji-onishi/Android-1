a26ccabffa770a4b973bc04d67bd7cd415f47260
山田 遼 <ryo_yamada@waku-2.com>
2018-07-25 13:41:33 +0900

    add LoadingIndicator

--- recipe_app/lib/ui/RecipePage.dart
+++ recipe_app/lib/ui/RecipePage.dart
@@ -11,11 +11,12 @@ class RecipePage extends StatefulWidget {
 
 class AllRecipePageState extends State<RecipePage> {
   List<RecipeModel> _items = [];
+  bool _loadingInProgress;
 
   @override
   void initState() {
     super.initState();
-
+    _loadingInProgress = true;
     new RecipeRepository(new RecipeApi())
         .findAll()
         .then((recipe) => setRecipe(recipe));
@@ -26,22 +27,34 @@ class AllRecipePageState extends State<RecipePage> {
     return MaterialApp(
       title: "Recipe App",
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text("Recipe App"),
-        ),
-        body: new ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (context, index) {
-            RecipeModel item = _items[index];
-            return RecipeItem(recipe: item);
-          },
-        ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.red,
+            title: Text("Recipe App"),
+          ),
+          body: _buildBody()),
     );
   }
 
+  Widget _buildBody() {
+    if (_loadingInProgress) {
+      return new Center(
+        child: new CircularProgressIndicator(),
+      );
+    } else {
+      return new ListView.builder(
+        itemCount: _items.length,
+        itemBuilder: (context, index) {
+          RecipeModel item = _items[index];
+          return RecipeItem(recipe: item);
+        },
+      );
+    }
+  }
+
   void setRecipe(List<RecipeModel> recipes) {
-    setState(() => _items = recipes);
+    setState(() {
+      _items = recipes;
+      _loadingInProgress = recipes.isEmpty;
+    });
   }
 }
