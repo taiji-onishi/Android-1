3d14616f22f3d7553af5587266b044a71e88abf3
ryo-yamada <ryo.yamada.miwa@gmail.com>
2018-07-30 02:12:27 +0900

    del import

--- recipe_app/lib/api/RecipeApi.dart
+++ recipe_app/lib/api/RecipeApi.dart
@@ -2,16 +2,14 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:recipe_app/model/CookingMethod.dart';
-import 'package:recipe_app/model/Ingredients.dart';
 import 'package:recipe_app/model/RecipeModel.dart';
 
 class RecipeApi {
   // 環境によって変更する
   // iOSのパス
-//  static const String _api_path = "http://127.0.0.1:3000/recipe";
+  static const String _api_path = "http://127.0.0.1:3000/recipe";
   // androidのパス
-  static const String _api_path = "http://10.0.2.2:3000/recipe";
+//  static const String _api_path = "http://10.0.2.2:3000/recipe";
 
   var _recipeMap = new Map<String, RecipeModel>();
 
