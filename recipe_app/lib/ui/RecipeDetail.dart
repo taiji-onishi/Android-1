11c3415ba57e6cd21ee568f0b59080ae7a362a9b
ryo-yamada <ryo.yamada.miwa@gmail.com>
2018-07-30 01:56:30 +0900

    fix DetailPage

--- recipe_app/lib/ui/RecipeDetail.dart
+++ recipe_app/lib/ui/RecipeDetail.dart
@@ -25,82 +25,90 @@ class RecipeDetailState extends State<RecipeDetail> {
         backgroundColor: Colors.red,
         title: Text(_recipeModel.recipeName),
       ),
-      body: new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: new ListView.builder(
+          itemCount: _ingredient.length + _methods.length + 2,
+          itemBuilder: (context, index) {
+            return _buildDetailItem(context, index);
+          }),
+    );
+  }
+
+  Widget _buildDetailItem(BuildContext context, int position) {
+    RecipeModel _recipeModel = widget.recipe;
+    List<Ingredients> _ingredient = _recipeModel.ingredients.values.toList();
+    List<CookingMethod> _methods = _recipeModel.cookingMethod.values.toList();
+    if (position == 0) {
+      return new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          // Image
           new Container(
             child: new Image.network(_recipeModel.photoUrl),
           ),
-          new Container(
-            padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-            child: new Text(
-              _recipeModel.introduction,
-              style: TextStyle(fontSize: 20.0),
+          new Center(
+            child: new Container(
+              padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+              child: new Text(
+                _recipeModel.introduction,
+                style: TextStyle(fontSize: 16.0),
+              ),
             ),
           ),
           new Container(
             padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
             child: new Text(
               "材料",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          new Expanded(
-            child: new ListView.builder(
-              primary: false,
-              itemCount: _ingredient.length,
-              itemBuilder: (context, index) {
-                return new Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0),
-                  child: new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      new Text(_ingredient[index].material),
-                      new Text(_ingredient[index].quantity),
-                    ],
-                  ),
-                );
-              },
-            ),
-          ),
-          
-          new Container(
-            padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
-            child: new Text(
-              "調理方法",
-              style: TextStyle(fontSize: 20.0),
-            ),
-          ),
-          new Expanded(
-            child: new ListView.builder(
-              primary: false,
-              itemCount: _methods.length,
-              itemBuilder: (context, index) {
-                return new Container(
-                  child: new Stack(
-                    children: <Widget>[
-                      new Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          new Text(_methods[index].no + "."),
-                          new Expanded(
-                            child: Text(_methods[index].procedure),
-                          ),
-                        ],
-                      ),
-                      new Divider(
-                        height: 2.0,
-                      ),
-                    ],
-                  ),
-                );
-              },
+              style: TextStyle(
+                fontSize: 16.0,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ],
-      ),
-    );
+      );
+    } else if (position < _ingredient.length + 1) {
+      return new Container(
+        padding: EdgeInsets.symmetric(horizontal: 8.0),
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Text(_ingredient[position - 1].material),
+            new Text(_ingredient[position - 1].quantity),
+          ],
+        ),
+      );
+    } else if (position < _ingredient.length + 2) {
+      return new Container(
+        padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0),
+        child: new Text(
+          "調理方法",
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+    } else if (position < _ingredient.length + _methods.length + 2) {
+      return new Container(
+        margin: EdgeInsets.only(bottom: 8.0, left: 8.0, right: 8.0),
+        child: new Stack(
+          children: <Widget>[
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                    _methods[position - (_ingredient.length + 2)].no + "."),
+                new Expanded(
+                  child: Text(
+                      _methods[position - (_ingredient.length + 2)].procedure),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+    return Container();
   }
 }
