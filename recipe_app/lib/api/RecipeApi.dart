c8c21d9ea680beeca1499a40ea2de0e2c2bfa219
山田 遼 <ryo_yamada@waku-2.com>
2018-07-31 10:33:54 +0900

    add getPath

--- recipe_app/lib/api/RecipeApi.dart
+++ recipe_app/lib/api/RecipeApi.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 import 'package:recipe_app/model/RecipeModel.dart';
@@ -7,9 +8,9 @@ import 'package:recipe_app/model/RecipeModel.dart';
 class RecipeApi {
   // 環境によって変更する
   // iOSのパス
-//  static const String _api_path = "http://127.0.0.1:3000/recipe";
+  static const String _ios_path = "http://127.0.0.1:3000/recipe";
   // androidのパス
-  static const String _api_path = "http://10.0.2.2:3000/recipe";
+  static const String _android_path = "http://10.0.2.2:3000/recipe";
 
   var _recipeMap = new Map<String, RecipeModel>();
 
@@ -19,12 +20,21 @@ class RecipeApi {
   }
 
   _request() async {
-    var response = await http.get(_api_path);
+    var response = await http.get(_getApiPath());
     var data = json.decode(response.body);
 
     // recipe
     for (var item in data) {
       _recipeMap[item['recipe_id']] = RecipeModel.fromJson(item);
     }
+  }
+
+  String _getApiPath(){
+    if (Platform.isAndroid){
+      return _android_path;
+    } else if (Platform.isIOS){
+      return _ios_path;
+    }
+    return null;
   }
 }
