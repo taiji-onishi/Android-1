a9e983ce0d423bc700b893e48a2cb56832ef0646
nyanc0 <y.fukuyuri@gmail.com>
2018-08-03 16:09:32 +0900

    不要なプロジェクト削除＆image追加

--- Practice/SampleApp/model/src/main/java/sampleapp/practice/com/example/yuriyuri/model/Tag.kt
+++
@@ -1,8 +0,0 @@
-package sampleapp.practice.com.example.yuriyuri.model
-
-data class Tag(
-        val followersCount: Int,
-        val iconImage: String,
-        val tagId: TagId,
-        val itemsCount: Int
-)
